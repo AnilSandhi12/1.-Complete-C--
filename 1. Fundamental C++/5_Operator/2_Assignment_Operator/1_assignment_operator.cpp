0b05476030095d334906d61f8b14141940df9a48
AnilSandhi12 <anilsandhi01@gmail.com>
2022-11-08 19:01:02 +0530

    add-files

---
+++ 1. Fundamental C++/5_Operator/2_Assignment_Operator/1_assignment_operator.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+cout<<"************** Relational Operator ******************\n";
+            int x=5;
+            int y=x;
+
+cout<<"************** [ = ] Relational Operator ******************\n";
+            cout<<"Y : "<<y<<endl;
+
+cout<<"************** [ += ] Relational Operator ******************\n";
+             x+=3;
+            cout<<"X : "<<x<<endl;
+
+cout<<"************** [ -= ] Relational Operator ******************\n";
+            x-=3;
+            cout<<"X : "<<x<<endl;
+
+cout<<"************** [ -= ] Relational Operator ******************\n";
+            x*=2;
+            cout<<"X : "<<x<<endl;
+
+cout<<"************** [ -= ] Relational Operator ******************\n";
+            x/=10;
+            cout<<"X : "<<x<<endl;
+
+cout<<"************** [ -= ] Relational Operator ******************\n";
+            x%=3;
+            cout<<"X : "<<x<<endl;
+
+cout<<"************** [ -= ] Relational Operator ******************\n";
+            x&=3; // x=5&3;
+            cout<<"X : "<<x<<endl;
+
+cout<<"************** [ -= ] Relational Operator ******************\n";
+            x|=3;
+            cout<<"X : "<<x<<endl;
+
+cout<<"************** [ -= ] Relational Operator ******************\n";
+            x^=3;
+            cout<<"X : "<<x<<endl;
+
+cout<<"************** [ -= ] Relational Operator ******************\n";
+            x>>=1;
+            cout<<"X : "<<x<<endl;
+
+cout<<"************** [ -= ] Relational Operator ******************\n";
+            x<<=1;
+            cout<<"X : "<<x<<endl;
+        return 0;
+}
